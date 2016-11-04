f9db008b81746419eafceb6d06322ff18a387f80
Bryan Benson <bryanbenson83@gmail.com>
2016-11-04 15:05:29 -0400

    Remove express

--- server.js
+++ server.js
@@ -1,14 +1,38 @@
-var express = require('express');
-var path = require('path');
-var app = express();
+var http = require('http')
+  , fs   = require('fs')
+  , url  = require('url')
+  , port = 8080;
 
-// Define the port to run on
-app.set('port', process.env.PORT || 8080);
+var server = http.createServer (function (req, res) {
+  var uri = url.parse(req.url)
 
-app.use(express.static(path.join(__dirname, 'public')));
+  switch( uri.pathname ) {
+    case '/':
+      sendFile(res, 'public/index.html', 'text/html', 200)
+      break
+    case '/index.html':
+      sendFile(res, 'public/index.html', 'text/html', 200)
+      break
+    case '/more_kittens.html':
+      sendFile(res, 'public/more_kittens.html', 'text/html', 200)
+      break
+    case '/css/style.css':
+      sendFile(res, 'public/css/style.css', 'text/css', 200)
+      break
+    case '/js/scripts.js':
+      sendFile(res, 'public/js/scripts.js', 'text/javascript', 200)
+      break
+    default:
+      sendFile(res, 'public/404.html', 'text/html', 404)
+  }
+})
 
-// Listen for requests
-var server = app.listen(app.get('port'), function() {
-  var port = server.address().port;
-  console.log('Server running on port ' + port);
-});
+server.listen(process.env.PORT || port);
+console.log('listening on 8080')
+
+function sendFile(res, filename, type, code) {
+  fs.readFile(filename, function(error, content) {
+    res.writeHead(code, {'Content-type': type})
+    res.end(content, 'utf-8')
+  })
+}
